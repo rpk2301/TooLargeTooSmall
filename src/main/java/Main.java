ca07884f4625f31cd5b6678cd01a2e9135e0f34c
ricky <rpk2301@vt.edu>
2023-06-27 21:10:36 -0400

    I have finished the lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,54 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+import java.util.Random;
+
+
 public class Main {
 
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
+
+        int guess = 0;
+        int count = 0;
+        int lastguess = 0;
+        Random rand = new Random();
+        int number = rand.nextInt(50);
+        Scanner scan = new Scanner(System.in);
+
+5
+        boolean correct = false;
+        while (correct == false)
+        {
+            System.out.println("Take a guess at the number!");
+            guess = scan.nextInt();
+            if (guess > number)
+            {
+                System.out.println("That number was too large");
+                if(guess!=lastguess)
+                {
+
+
+                    count++;
+                    lastguess=guess;
+                }
+            }
+            else if (guess < number)
+            {
+                System.out.println("That number was too small");
+                if(guess!=lastguess)
+                {
+                    count++;
+                    lastguess=guess;
+                }
+            }
+            else
+            {
+                System.out.println("Congrats! You guessed the correct number");
+                System.out.println("You took: " + count + " tries!");
+                correct = true;
+            }
+        }
     }
 }
+
+
